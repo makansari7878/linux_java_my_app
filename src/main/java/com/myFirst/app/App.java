32e0f77cb71a813fbf32f553c969cfd4a0902c8e
root <root@ip-172-31-41-27.ap-south-1.compute.internal>
2023-04-13 09:43:47 +0000

    First commit

---
+++ src/main/java/com/myFirst/app/App.java
@@ -0,0 +1,15 @@
+package com.myFirst.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      System.out.println( "Hello Ansari!" );
+
+        System.out.println( "Hellon n welcome to my  World!" );
+    }
+}
